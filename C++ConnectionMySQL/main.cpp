18b25fc26dab2637e36acea2efdf7060df9e6935
wangsijun <wangsijun1986@vip.qq.com>
2015-12-19 13:22:47 +0800

    Initial Commit

---
+++ C++ConnectionMySQL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ConnectionMySQL
+//
+//  Created by 王思俊 on 15/12/19.
+//  Copyright © 2015年 王思俊. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
